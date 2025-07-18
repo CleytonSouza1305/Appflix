7f55d2c4ea5ee021d05e411585420c7ea34493ce
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-07-18 18:20:33 -0300

    Criando a função que buscar os perfis e cria-os

---
+++ src/js/browse.js
@@ -0,0 +1,104 @@
+const token = localStorage.getItem('token')
+
+async function searchProfile(token) {
+  const loader = document.getElementById('loading')
+  loader.classList.remove('display')
+
+  try {
+    const response = await fetch(`https://appflix-api.onrender.com/api/profiles`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    if (!response.ok) {
+      throw new Error(response.message)
+    }
+
+    const data = await response.json()
+    return data
+
+  } catch (e) {
+    console.error(`Erro na requisição, motivo: ${e}`)
+    location.href = './login.html'
+  } finally {
+    loader.classList.add('display')
+  }
+}
+
+function createCardProfile(data) {
+  const container = document.querySelector('.profile-content')
+  if (!container) {
+    console.error('Container não encontrado.')
+    return
+  }
+
+  container.innerHTML = ''
+
+  for (let i = 0; i < data.length; i++) {
+    const { id, avatarUrl, profileName, profilePin } = data[i]
+
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const infoContainer = document.createElement('div')
+    infoContainer.classList.add('info-container')
+    infoContainer.dataset.profile = id
+
+    const contentImage = document.createElement('div')
+    contentImage.classList.add('content-image')
+
+    const img = document.createElement('img')
+    img.src = avatarUrl
+    img.alt = profileName
+
+    const name = document.createElement('p')
+    name.classList.add('profile-name')
+    name.innerText = profileName
+
+    contentImage.appendChild(img)
+    infoContainer.append(contentImage, name)
+    card.appendChild(infoContainer)
+
+    const contentBottom = document.createElement('div')
+    contentBottom.classList.add('bottom-container')
+
+    if (profilePin !== null) {
+      const lockIcon = document.createElement('i')
+      lockIcon.className = 'fa-solid fa-lock'
+
+      contentBottom.appendChild(lockIcon)
+    }
+
+    card.appendChild(contentBottom)
+    container.appendChild(card)
+  }
+
+
+  if (data.length < 5) {
+    const moreProfile = document.createElement('div')
+    moreProfile.classList.add('more-profile-btn')
+
+    moreProfile.innerHTML = `<i class="fa-solid fa-plus"></i>`
+    container.append(moreProfile)
+  }
+
+  console.log(data)
+}
+
+async function startApp(token) {
+  const data = await searchProfile(token)
+  createCardProfile(data)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const token = localStorage.getItem('token')
+
+  if (!token) {
+    location.href = './login.html'
+  } else {
+    startApp(token)
+  }
+})
