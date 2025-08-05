f94feb1c9dbeb9fda06a081c4bf428f9d99cdd1c
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-08-05 16:05:38 -0300

    Criando a função que busca todos os perfis e filtrando-os

--- src/js/profile.js
+++ src/js/profile.js
@@ -30,9 +30,68 @@ async function profileData(token, id) {
   }
 }
 
+async function searchProfile(token) {
+  const loader = document.getElementById("loading");
+  loader.classList.remove("display");
+
+  try {
+    const response = await fetch(
+      `https://appflix-api.onrender.com/api/profiles`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(response.message);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.error(`Erro na requisição, motivo: ${e}`);
+    location.href = "./login.html";
+  } finally {
+    loader.classList.add("display");
+  }
+}
+
+function insertProfileData(data, allProfiles) {
+  console.log(data)
+  console.log(allProfiles)
+
+  const avatar = data.avatar_link
+
+  const avatarImage = document.getElementById('avatar-image')
+  avatarImage.src = avatar
+
+  const profiles = allProfiles.filter((profile) => profile.id !== data.id)
+  console.log(profiles)
+
+  const content = document.querySelector('.user-data-content')
+
+  const contentAvatar = document.querySelector('.user-avatar')
+  contentAvatar.addEventListener('mouseenter', (el) => {
+    content.classList.remove('display')
+
+    console.log(el.currentTarget)
+  })
+
+  contentAvatar.addEventListener('mouseleave', (ev) => {
+    setTimeout(() => {
+      content.classList.add('display')
+    }, 2 * 1000);
+  })
+}
+
 async function startApp(token, profileId) {
   const data = await profileData(token, profileId)
-  
+  const profiles = await searchProfile(token)
+  insertProfileData(data, profiles)
 }
 
 async function validateToken(token, path) {
